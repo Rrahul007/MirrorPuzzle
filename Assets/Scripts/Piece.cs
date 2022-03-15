c4d32f6c1dd990a1d2178caf8e89e1ee8674bdba
Rahul Adke <rsadke007@gmail.com>
2022-03-15 13:08:30 +0530

    Add files via upload

---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    /// <summary>
+    /// Class Piece contains the functionality of broken piece
+    /// </summary>
+
+    [SerializeField]
+    private Transform piecePlace;   // Variable for holding the correct targeted position of the broken piece
+
+    private float deltaX, deltaY;   // New position variable as per piece movement
+
+    public bool locked;             // Status variable for locking the piece movement if piece is dropped at correct place
+
+    public AudioSource matchedSound;    // Audiosource to play on successfull drop
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && !locked)  // To check if input touch is more than 0, and is not locked for movement
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began: // Executes the block as user touch get started the screen
+                    if (UIManager.isMove)   // Check if control is on move or on rotate
+                    {
+                        if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos)) // Getting touch position if touched on particular piece collider
+                        {
+                            deltaX = touchPos.x - transform.position.x;
+                            deltaY = touchPos.y - transform.position.y;
+                        }
+                    }
+                    else
+                    {
+                        if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+                        {
+                            transform.Rotate(0f, 0f, 90f);
+                        }
+                    }
+                    break;
+
+                case TouchPhase.Moved:
+                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+                        transform.position = new Vector2(touchPos.x - deltaX, touchPos.y - deltaY);     // Move piece on new position
+                    break;
+
+                case TouchPhase.Ended:      // If touch ended leave the piece on that position and check if it is target position
+                    //if(this.tag == piecePlace.tag)
+                    if (Mathf.Abs(transform.position.x - piecePlace.position.x) <= 0.5f &&
+                        Mathf.Abs(transform.position.y - piecePlace.position.y) <= 0.5f && transform.rotation.z == 0f)
+                    {
+                        transform.position = new Vector2(piecePlace.position.x, piecePlace.position.y);
+                        locked = true;
+                        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+                        UIManager.pieceMatched++;
+                        matchedSound.Play();    // Play sound if target position is matched
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public static bool isMove;      // Variable to set move or rotate control of piece
+    public AudioSource winSound;    // Sound variable for winning sound
+    public static int pieceMatched; // Number of piece matched
+    public GameObject cleanImage;   // Clean image of solved puzzle
+    public Text winText;            // Greeting text on winning
+
+    // Start is called before the first frame update
+    void Start()        // To initialize the primary values of data members on game start
+    {
+        cleanImage.SetActive(false);
+        winText.enabled = false;
+        pieceMatched = 0;
+        isMove = true;
+    }
+
+    private void Update()
+    {
+        if(pieceMatched == 9)   // Check if all the pieces are matched and then react according
+        {
+            cleanImage.SetActive(true);
+            winText.enabled = true;
+            winSound.Play();
+            pieceMatched = 0;
+        }
+    }
+
+    public void RotatePiece()   // To set rotate control
+    {
+        isMove = false;
+    }
+
+    public void MovePiece()     // To set move control
+    {
+        isMove = true;
+    }
+
+    public void RetryGame()     // Restart the game
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitGame()      // Quit the game application
+    {
+        Application.Quit();
+    }
+}
